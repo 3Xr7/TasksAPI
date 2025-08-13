bbac6ec1595da448abe89dcb0d85abfbaf2931da
3Xr7 <32613196+3Xr7@users.noreply.github.com>
2025-08-13 20:09:07 +0100

    Add basic test from all endpoints
    The majority of this commit is adding the required functions and
    fixtures to conftests for the test cases to achieve the intended
    outcome. The added test cases simply test the basic expected happy path
    of the requirements, next steps for them would be to parametrize those
    happy paths for further verification followed by the additional of cases
    to cover fault paths.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 import os
+import random
 
 from datetime import datetime, timedelta
 
@@ -13,10 +14,42 @@ sqlite_url = f"sqlite:///{sqlite_file_name}"
 connect_args = {"check_same_thread": False}
 engine = create_engine(sqlite_url, connect_args=connect_args)
 
-mock_due_data = (datetime.now() + timedelta(days=1))
+session = Session(engine)
 
-def str_format_due_date(date: datetime):
+
+def mock_due_date(days_in_future: int = 1) -> datetime:
+  """
+    Generates a future mock date that is now plus days_in_future
+
+    Parameters
+    ----------
+    days_in_future : int
+        The number of days to add to now.
+
+    Returns
+    -------
+    datetime
+        The current date time plus days_in_future
+  """
+  return (datetime.now() + timedelta(days=days_in_future))
+
+
+def str_format_due_date(date: datetime) -> str:
+  """
+    Converts the supplied datetime as a string with format '%Y-%m-%dT%H:%M:%S.%f'
+
+    Parameters
+    ----------
+    date : datetime
+        The datetime to convert to a string.
+
+    Returns
+    -------
+    str
+        The supplied datetime as a string.
+  """
   return date.strftime('%Y-%m-%dT%H:%M:%S.%f')
+
 
 seed_tasks = [
   {
@@ -24,24 +57,24 @@ seed_tasks = [
     'description': None,
     'priority': 3,
     'completed': False,
-    'due_date': mock_due_data
+    'due_date': mock_due_date()
   },
   {
     'title': 'bar',
     'description': None,
     'priority': 2,
     'completed': False,
-    'due_date': mock_due_data
+    'due_date': mock_due_date()
   }
 ]
 
-@pytest.fixture(scope='session')
-def session():
-  return Session(engine)
-
 
 @pytest.fixture(autouse=True, scope='session')
-def seed_db(session):
+def seed_db():
+  """
+    A fixture that handles seeding the test database with data and the teardown
+    of the table at the end of the test session.
+  """
   SQLModel.metadata.create_all(engine)
 
   for task in seed_tasks:
@@ -55,7 +88,21 @@ def seed_db(session):
   SQLModel.metadata.drop_all(bind=engine)
 
 
-def task_to_dict(task):
+def task_to_dict(task: Task) -> dict:
+  """
+    Converts the supplied Task object to a dictionary with the due_date field
+    also converted to a string.
+
+    Parameters
+    ----------
+    task : Task
+        The Task to convert to a dictionary.
+
+    Returns
+    -------
+    dict
+        The supplied Task as a dictionary.
+  """
   d = {}
   for column in task.__table__.columns:
     if column.name == 'due_date':
@@ -65,7 +112,106 @@ def task_to_dict(task):
   return d
 
 
-@pytest.fixture(scope='session')
-def all_tasks(session):
+@pytest.fixture()
+def all_tasks() -> list[dict]:
+  """
+    A pytest fixture that looks up all tasks stored in the test database.
+
+    Returns
+    -------
+    list[dict]
+        A list of tasks as dictionaries returned from the test database.
+  """
   return [task_to_dict(t) for t in session.exec(select(Task)).all()]
 
+
+def all_task_ids() -> list[int]:
+  """
+    Looks up all task ids stored in the test database.
+
+    Returns
+    -------
+    list[int]
+        A list of task ids returned from the test database.
+  """
+  return session.exec(select(Task.id)).all()
+
+
+def get_task_by_id(id: int) -> dict:
+  """
+    Queries the test database for a task where id equals the supplied id int.
+
+    Parameters
+    ----------
+    id : int
+        The task id to look up in the test database.
+
+    Returns
+    -------
+    dict
+        The found task as a dictionary.
+  """
+  task = session.exec(select(Task).where(Task.id == id)).first()
+  return task_to_dict(task)
+
+
+def random_task_title_description() -> dict:
+  """
+    A function to get a randomly selected task title and description from a
+    predefined list generated using Lumo AI (https://lumo.proton.me). I asked an
+    AI to get the titles and description because I didn't want to think of them
+    myself. 
+
+    Returns
+    -------
+    dict
+        A randomly selected title and description dictionary.
+  """
+  task_title_descriptions = [
+    {
+      "title": "Essence of Existence",
+      "description": "The essence of existence transcends mere physical manifestation and perception."
+    },
+    {
+      "title": "Quantum Spacetime Dynamics",
+      "description": "Quantum fluctuations reveal the underlying fabric of spacetime dynamics."
+    },
+    {
+      "title": "Emergence of Consciousness",
+      "description": "Consciousness emerges from intricate neural networks and cognitive processes."
+    },
+    {
+      "title": "Philosophical Inquiry",
+      "description": "Philosophical inquiry explores the nature of truth and reality."
+    },
+    {
+      "title": "Abstract Art Challenges",
+      "description": "Abstract art challenges conventional perceptions of form and meaning."
+    },
+    {
+      "title": "Cosmic Echoes",
+      "description": None
+    }
+  ]
+  return task_title_descriptions[random.randint(0, 4)]
+ 
+
+@pytest.fixture()
+def task_dictionary() -> dict:
+  """
+    Generates a semi random Task as a dictionary via a combination of randomly
+    generated ints.
+
+    Returns
+    -------
+    dict
+        A dictionary representation of a Task.
+  """
+  title_desc = random_task_title_description()
+  return {
+    'title': title_desc["title"],
+    'description': title_desc["description"],
+    'priority': random.randint(1, 3),
+    'completed': False,
+    'due_date': str_format_due_date(mock_due_date(random.randint(1, 5)))
+  }
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,13 +1,59 @@
+import random
+
 from fastapi.testclient import TestClient
-
 from src.main import app
-
+from tests.conftest import all_task_ids, get_task_by_id
 
 client = TestClient(app)
 
 
 def test_get_tasks(all_tasks):
   resp = client.get("/tasks/")
-  print(resp.json())
   assert resp.status_code == 200
   assert resp.json() == all_tasks
+
+
+def test_create_task(task_dictionary):
+  new_task = {
+    "title": task_dictionary["title"],
+    "description": task_dictionary["description"],
+    "priority": task_dictionary["priority"],
+    "due_date": task_dictionary["due_date"]
+  }
+
+  resp = client.post("/tasks/", json=new_task)
+  task_created = resp.json()
+  task_ids = all_task_ids()
+
+  assert task_created["id"] == max(task_ids)
+  assert task_created["title"] == task_dictionary["title"]
+  assert task_created["description"] == task_dictionary["description"]
+  assert task_created["priority"] == task_dictionary["priority"]
+  assert task_created["due_date"] == task_dictionary["due_date"]
+  assert task_created["completed"] == False
+
+
+def test_update_task(task_dictionary):
+  task_ids = all_task_ids()
+  task_id = random.randint(min(task_ids), max(task_ids))
+
+  update_task = get_task_by_id(task_id)
+  update_task["title"] = task_dictionary["title"]
+  update_task["description"] = task_dictionary["description"]
+  update_task["completed"] = True
+
+  resp = client.put(f"/tasks/{task_id}", json=update_task)
+  task_updated = resp.json()
+
+  assert task_updated["title"] == task_dictionary["title"]
+  assert task_updated["description"] == task_dictionary["description"]
+  assert task_updated["completed"] == True
+
+
+def test_delete_task():
+  task_id = max(all_task_ids())
+
+  resp = client.delete(f"/tasks/{task_id}")
+
+  assert "message" in resp.json().keys()
+  assert resp.json()["message"] == "Task deleted successfully."
